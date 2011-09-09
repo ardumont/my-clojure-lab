86cf25c3c2b47c52fcf139dc88fd78852c2cf783
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-09-09 20:17:19 +0200

    Add some default useful namespaces.

--- src/my_clojure_lab/core.clj
+++ src/my_clojure_lab/core.clj
@@ -1,7 +1,11 @@
 (ns my-clojure-lab.core
-  (:use midje.sweet))
+   (:use [clojure.test               :only [run-tests]])
+   (:use [midje.sweet])
+   (:use [clojure.contrib.repl-utils :only [show]])
+   (:use [clojure.pprint             :only [pprint]])
+   (:use [clojure.walk               :only [macroexpand-all]]))
+)
 
-;.;. Of course the universe *is* out to get us, but it's not going to do it
-;.;. by passing a null to one of our methods. -- Ron Jeffries
+;.;. There's a certain satisfaction in a little bit of pain. -- Madonna
 (fact (+ 1 1) => 2)
 
